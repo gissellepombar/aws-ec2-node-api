2ffbe0cda41f9f23a0e31a0abf7d242345722633
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-23 16:03:07 -0500

    second commit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import 'dotenv/config'
 
 const app = express();
 app.use(cors());
-// app.use(express.json())
+app.use(express.json())
 
 app.listen('5002', () => console.log('listening on port 5002')) 
 
 app.get('/', (req, res) => { 
-    res.send('You did it! 🥳'); 
+    res.json('You did it! 🥳'); 
   });
